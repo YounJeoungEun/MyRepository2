c731e786953ed5f8de2ef460d85536e63ecef781
younje <younje@DESKTOP-M103A3B>
2021-06-12 21:17:06 +0900

    devB branch

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -3,6 +3,8 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello A");
+		System.out.println("Hello A2");
+		new B().sayHello();
 
 	}
 
--- MyGit Java/src/B.java
+++ MyGit Java/src/B.java
@@ -1,8 +1,9 @@
 
 public class B {
-
-	public static void main(String[] args) {
-		System.out.println("Hello B");
-	}
+	
+		public void sayHello() {
+			System.out.println("Hello B");
+		}
+		
 
 }
